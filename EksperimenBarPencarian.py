e05306a0cf25c6a4f1f2de2235f9071318b0aa76
Ardel7-dev <tsabitah365@gmail.com>
2025-12-16 01:00:49 +0700

    Revisi dengan memisahkan program pembaca resep dan pencari resep sebagai module berbeda, import module

--- EksperimenBarPencarian.py
+++ EksperimenBarPencarian.py
@@ -1,40 +1,18 @@
-def baca_resep_dari_file(nama_file):
-    resep_list = []
+from ModulBacaResep import BacaResepDariFile
+from ModulPencariResep import CariResep
 
-    with open(nama_file, "r", encoding="utf-8") as f:
-        blok = f.read().strip().split("\n\n")
+def Pencarian():
+    Resep = BacaResepDariFile("resep.txt")
 
-    for b in blok:
-        baris = b.split("\n")
-        nama = baris[0]
-        langkah = baris[1:]
+    KataKunci = input("Cari resep yang Anda inginkan: ")
+    Hasil = CariResep(KataKunci, Resep)
 
-        resep_list.append({
-            "nama": nama,
-            "langkah": langkah
-        })
+    if Hasil:
+        for r in Hasil:
+            print("\n" + r["Nama"])
+            for Langkah in r["Langkah"]:
+                print(Langkah)
+    else:
+        print("Resep tidak ditemukan.")
 
-    return resep_list
-
-def cari_resep(keyword, daftar_resep):
-    hasil = []
-    keyword = keyword.lower()
-
-    for resep in daftar_resep:
-        if keyword in resep["nama"].lower():
-            hasil.append(resep)
-
-    return hasil
-
-resep = baca_resep_dari_file("resep.txt")
-
-kata_kunci = input("Cari resep: ")
-hasil = cari_resep(kata_kunci, resep)
-
-if hasil:
-    for r in hasil:
-        print("\n", r["nama"])
-        for langkah in r["langkah"]:
-            print(langkah)
-else:
-    print("Resep tidak ditemukan.")
+Pencarian()
